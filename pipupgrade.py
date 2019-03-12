0d03d77ebb671609f20ce9e5659b578c7ac58661
thomaz sachetto <46068813+tsachetto@users.noreply.github.com>
2019-03-12 20:08:07 -0300

    Update pipupgrade.py

--- pipupgrade.py
+++ pipupgrade.py
@@ -1,5 +1,5 @@
-Primeiro: instala o pipupgrade (atualizador de packages)
-Segundo: utiliza o pipupgrade, atualizando todos os packages, inclusive instalando dependências importantes.
+#Primeiro: instala o pipupgrade (atualizador de packages)
+#Segundo: utiliza o pipupgrade, atualizando todos os packages, inclusive instalando dependências importantes.
 
 pip install pipupgrade
 pipupgrade --latest --yes
