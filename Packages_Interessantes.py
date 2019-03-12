2a3a789816ebb4ff01ffe2ef86fc198a376b6dd9
thomaz sachetto <46068813+tsachetto@users.noreply.github.com>
2019-03-12 10:43:45 -0300

    Create Packages_Interessantes.py

---
+++ Packages_Interessantes.py
@@ -0,0 +1,30 @@
+Packages interessantes de Python:
+
+----------------------------
+EXCEL:
+
+openpyxl
+
+----------------------------
+Atualizações PIP:
+
+pipupgrade
+
+----------------------------
+SQL & EXCELL:
+
+SQLAlchemy
+sqlexceller
+
+----------------------------
+PDF:
+
+PyPDF2 
+----------------------------
+Automations:
+
+pywinauto
+----------------------------
+
+
+
