355887f6889d06890acdb1fff930b77018553465
thomaz sachetto <46068813+tsachetto@users.noreply.github.com>
2019-03-12 10:35:58 -0300

    Create pipupgrade.py

---
+++ pipupgrade.py
@@ -0,0 +1,5 @@
+Primeiro: instala o pipupgrade (atualizador de packages)
+Segundo: utiliza o pipupgrade, atualizando todos os packages, inclusive instalando dependências importantes.
+
+pip install pipupgrade
+pipupgrade --latest --yes
