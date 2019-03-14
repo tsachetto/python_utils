89b2367dfdf3a9a30703ffcbf7a2d8420067574c
thomaz sachetto <46068813+tsachetto@users.noreply.github.com>
2019-03-13 23:21:10 -0300

    Create Fatorial.py

---
+++ Fatorial.py
@@ -0,0 +1,21 @@
+#Duas funções lindas que calculam o fatorial de um número. Não autoral.
+
+def get_recursive_factorial(n):
+	if n < 0:
+		return -1
+	elif n < 2:
+		return 1
+	else:
+		return n * get_recursive_factorial(n-1)
+		
+def get_iterative_factorial(n):
+	if n < 0:
+		return -1
+	else:
+		fact = 1
+		for i in range(1, n+1):
+			fact *= i
+		return fact
+		
+print(get_recursive_factorial(10))
+print(get_iterative_factorial(10))
