0ca08e839eec32061fae78cc270cb2fb0385ece8
thomaz sachetto <46068813+tsachetto@users.noreply.github.com>
2019-03-17 23:56:29 -0300

    Create Somar_Digitos_Permut.py

---
+++ Somar_Digitos_Permut.py
@@ -0,0 +1,22 @@
+#soma os algarismos (dígitos) de um número e os escreve em um txt caso preencham uma condição:
+#código autoral by tsachetto ;)
+
+import itertools as its
+
+f = open("C:\\Py\\logpy2.txt", "w")
+for i in range(1,10):
+
+    a = its.permutations('123456789',i)
+
+    for t in a:
+        xx = 0
+        soma = (''.join(str(s) for s in t))
+        somax = (''.join(str(s) for s in t) + '\n')
+
+        for n in soma:
+            xx += int(n)
+
+        if xx == 7:
+            f.write(soma + ' - ' + str(xx) + '\n')
+
+f.close()
